dc959b0f799685b688269d6c4e75c3ae1e5290ae
Екатерина Антонова <karafiat@mail.ru>
2022-05-12 10:49:00 +0700

    added tests with annotations before, after

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,43 @@
+package guru.qa;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+
+    @BeforeAll
+    static void initDB () {
+        System.out.println("### @BeforeAll");
+    }
+
+    @BeforeEach
+    void openYaPage() {
+        System.out.println("###     @BeforeEach");
+        //Selenide.open("https://ya.ru");
+    }
+    @AfterEach
+    void closePage() {
+        System.out.println("###     @AfterEach");
+        //WebDriverRunner.closeWindow();
+    }
+
+    @AfterAll
+    static void cleanDB () {
+        System.out.println("### @AfterAll");
+    }
+
+    @Test
+    void assertTest() {
+        System.out.println("###         @Test0");
+        Assertions.assertTrue(5 <= 5);
+    }
+
+    @Test
+    void assertTest2() {
+        System.out.println("###         @Test1");
+        Assertions.assertTrue(10 < 11);
+    }
+
+}
+
