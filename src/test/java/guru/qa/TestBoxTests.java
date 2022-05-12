04812b72dadf39cc35ef73ce40c32452cc31de7f
Екатерина Антонова <karafiat@mail.ru>
2022-05-12 18:44:22 +0700

    added tests fot Text Fields (name, email, current, permanent address

---
+++ src/test/java/guru/qa/TestBoxTests.java
@@ -0,0 +1,50 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+//        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1600x1000";
+    }
+
+    @Test
+    void fillFormTest() {
+        String name = "Pavlikov Danil";
+        String email = "Pavlikov@Danil.com";
+        String currentAddress = "Some street 1";
+        String permanentAddress = "Some city 2";
+
+        open("/text-box");
+        $("#userName").setValue(name);
+        $("#userEmail").setValue(email);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        //Asserts
+        $("#output").shouldHave(
+                text(name),
+                text(email),
+                text(currentAddress),
+                text(permanentAddress)
+        );
+//        $("#output #name").shouldHave(text(name));
+//        $("#output").$("#email").shouldHave(text(email));
+//        //$("#output #currentAddress").shouldHave(text(currentAddress));
+//        $("[id=permanentAddress]").shouldHave(text("Permananet Address :Some city 2")); //wrong
+//        $("#output").$("[id=permanentAddress]").shouldHave(text("Permananet Address :Some city 2"));
+//        $("[id=permanentAddress]", 1).shouldHave(text("Permananet Address :Some city 2"));
+//        $("p[id=permanentAddress]").shouldHave(text("Permananet Address :Some city 2"));
+    }
+}
